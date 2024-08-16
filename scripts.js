9ea9f316945e491074fccd19deaf15640efe211c
chypa123 <rastorguyeva.t@bk.ru>
2024-08-16 14:33:20 +0300

    все

--- scripts.js
+++ scripts.js
@@ -1,19 +1,44 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.grid-item img').forEach(img => {
         img.addEventListener('click', (e) => {
             const content = e.target.getAttribute('data-content');
             document.getElementById('popup').style.display = 'flex';
             document.getElementById('popup-inner-content').innerHTML = content;
+            document.body.classList.add('modal-open'); // Блокируем прокрутку
         });
     });
 
-    document.getElementById('close-btn').addEventListener('click', () => {
+    const closeModal = () => {
         document.getElementById('popup').style.display = 'none';
-    });
+        document.body.classList.remove('modal-open'); // Включаем прокрутку
+    };
+
+    document.getElementById('close-btn').addEventListener('click', closeModal);
 
     document.getElementById('popup').addEventListener('click', (e) => {
         if (e.target === document.getElementById('popup')) {
-            document.getElementById('popup').style.display = 'none';
+            closeModal();
         }
     });
+
+    const images = document.querySelectorAll('.grid-item img');
+    popup.addEventListener('click', (e) => {
+        const popupImages = document.querySelectorAll('.popup-img');
+        let clickInsideImage = false;
+
+        popupImages.forEach(image => {
+            if (image.contains(e.target)) {
+                clickInsideImage = true;
+            }
+        });
+
+        if (!clickInsideImage) {
+            popup.style.display = 'none';
+            document.body.classList.remove('modal-open');
+        }
+    });
+    
+
 });
+
